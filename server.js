19fd7cf877e04ad5927ff47ee5d13e7e79d654b5
Yang <9488372@hanmail.net>
2022-03-19 10:10:26 +0900

    npm install nodemon 자동스타트

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ import express from "express"
 
 const app = express()
 
+app.get("/", (req, res) => {
+    res.json({
+        message : "hi test node"
+    })
+})
+
 const PORT = 9000
 
 app.listen(PORT, () => console.log("server started"))
