412677669cc65954eacc086f7383b54ec9fbd767
Yang <9488372@hanmail.net>
2022-03-23 21:05:27 +0900

    add login

---
+++ models/userModel.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+    {
+        name : {
+            type: String,
+            required: true
+        },
+        email :{
+            type: String,
+            required: true
+        },
+        password :{
+            type: String,
+            required: true
+        },
+        phone: {
+            type: Number,
+            // required: true
+        },
+        bio: {
+            type: String,
+            // required: true
+        },
+        address: {
+            type: String,
+            // required: true
+        }
+    },
+    {
+        timestamps : true
+    }
+)
+
+
+const User = mongoose.model("User", userSchema)
+
+export default User
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,76 @@
 import express from "express"
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+import bcrypt from "bcryptjs"
 
 const router = express.Router()
 
+
+// 회원가입
+router.post("/signup", asyncHandler(async (req, res) => {
+    const { name, email, password } = req.body
+
+    // 이메일 유/무 체크
+    // 패스워드 암호화
+
+
+    const user = await User.findOne({ email })
+
+    if (user) {
+        return res.status(404).json({
+            msg: "user already existed"
+        })
+
+    }else{
+
+        // 패스워드 암호화
+        const passwordHashed = await bcrypt.hashSync(password, 10)
+
+
+        const user = new User({
+            name, email, password: passwordHashed
+        })
+
+        const newUser = await user.save();
+
+        res.json({
+            message: "create a user",
+            user : newUser
+        })
+    }
+
+
+
+}))
+
+// 로그인
+router.post("/login", asyncHandler( async (req, res) => {
+
+    const { email, password } = req.body
+
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        return res.status(404).json({
+            msg :  "email is not registered"
+        })
+    }else{
+       const isMatched = await bcrypt.compare(password, user.password)
+        if(!isMatched){
+            return res.status(408).json({
+                msg : "password is not matched"
+            })
+        }else{
+            res.json(user)
+        }
+    }
+
+
+
+}))
+
+
+// 프로필 불러오기
 router.get("/", (req, res) => {
     res.json({
         message:"user get"
