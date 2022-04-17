2d0cbbe78fd74b7b0cbceff948751447caab738e
Yang <9488372@hanmail.net>
2022-04-17 10:01:51 +0900

    add productController

---
+++ controllers/productController.js
@@ -0,0 +1,121 @@
+import asyncHandler from "express-async-handler";
+import Product from "../models/productModel.js";
+
+
+const getProducts = asyncHandler (async (req, res) => {
+    const products = await Product.find()
+    res.json({
+        count: products.length,
+        products: products.map(product => {
+            return {
+                id: product._id,
+                name: product.name,
+                price: product.price,
+            }
+        })
+    })
+})
+
+const getProductById = asyncHandler(async (req,res) => {
+
+    const {productId} = req.params
+    // const id = req.params.productId
+
+    const product = await Product.findById(productId)
+
+    if (product){
+        res.json({
+            id:product._id,
+            name:product.name,
+            price:product.price,
+            brand:product.brand,
+            category:product.category,
+        })
+    }else{
+        res.status(404)
+        throw new Error('Product not found')
+    }
+
+})
+
+const createProduct = asyncHandler (async (req, res) =>{
+
+    const {name, price, brand, category, description} = req.body
+
+    const product = new Product({
+        name,
+        price,
+        brand,
+        category,
+        description,
+    })
+
+    const newProduct =  await product.save()
+
+    res.json({
+        message : "created a product",
+        product : newProduct
+    })
+
+
+})
+
+const putProduct = asyncHandler(async (req, res) =>{
+
+    const {productId} = req.params
+
+    const {name, price, brand, category, description} = req.body
+
+    const product = await Product.findById(productId)
+
+    if (product) {
+        product.name = name || product.name
+        product.price = price || product.price
+        product.brand = brand || product.brand
+        product.category = category || product.category
+        product.description = description || product.description
+
+        const updatedProduct = await product.save()
+
+        res.json({
+            msg : "updated product",
+            product : updatedProduct
+        })
+
+    }else{
+        res.status(404)
+        throw new Error('Product not Found')
+    }
+
+
+
+})
+
+const deleteProduct = asyncHandler(async (req,res) => {
+
+    await Product.remove()
+
+    res.json({
+        msg : "deleted all product"
+    })
+})
+
+const deleteProductDetail = asyncHandler(async (req,res) => {
+
+    const {productId} = req.params
+    // const id = req.params.productId
+    await Product.findByIdAndRemove(productId)
+
+    res.json({
+        msg : "deleted at "+ productId
+    })
+
+})
+
+
+export { getProducts,
+         getProductById,
+         createProduct,
+         putProduct,
+         deleteProduct,
+         deleteProductDetail }
--- routes/product.js
+++ routes/product.js
@@ -2,133 +2,33 @@ import express from "express"
 import Product from "../models/productModel.js";
 import asyncHandler from "express-async-handler";
 
+import {
+    getProducts,
+    getProductById,
+    createProduct,
+    putProduct,
+    deleteProduct,
+    deleteProductDetail
+} from "../controllers/productController.js";
+
 const router = express.Router()
 
 // crud
 // 전체 product 불러오는 api
-router.get("/", asyncHandler (async (req, res) => {
-   const products = await Product.find()
-    res.json({
-        count: products.length,
-        products: products.map(product => {
-            return {
-                id: product._id,
-                name: product.name,
-                price: product.price,
-            }
-        })
-    })
-}))
+router.get("/", getProducts)
 
 // 상세 product 불러오는 api
-router.get("/:productId", asyncHandler(async (req,res) => {
-
-    const {productId} = req.params
-    // const id = req.params.productId
-
-    const product = await Product.findById(productId)
-
-    if (product){
-        res.json({
-            id:product._id,
-            name:product.name,
-            price:product.price,
-            brand:product.brand,
-            category:product.category,
-        })
-    }else{
-        res.status(404)
-        throw new Error('Product not found')
-    }
-
-}))
+router.get("/:productId", getProductById)
 
 
-router.post("/", asyncHandler (async (req, res) =>{
+router.post("/", createProduct )
 
-    // const userInput = {
-    //     name : req.body.name,
-    //     price : req.body.price,
-    //     desc : req.body.description
-    // }
-    //
-    // res.json({
-    //     msg : "product create",
-    //     newProduct : userInput
-    // })
-
-    const {name, price, brand, category, description} = req.body
-
-    const product = new Product({
-        name,
-        price,
-        brand,
-        category,
-        description,
-    })
-
-    const newProduct =  await product.save()
-
-    res.json({
-        message : "created a product",
-        product : newProduct
-    })
-
-
-}))
-
-router.put("/:productId",asyncHandler(async (req, res) =>{
-
-    const {productId} = req.params
-
-    const {name, price, brand, category, description} = req.body
-
-    const product = await Product.findById(productId)
-
-    if (product) {
-        product.name = name || product.name
-        product.price = price || product.price
-        product.brand = brand || product.brand
-        product.category = category || product.category
-        product.description = description || product.description
-
-        const updatedProduct = await product.save()
-
-        res.json({
-            msg : "updated product",
-            product : updatedProduct
-        })
-
-    }else{
-        res.status(404)
-        throw new Error('Product not Found')
-    }
-
-
-
-}))
+router.put("/:productId", putProduct)
 
 //product 전체 delete
-router.delete( "/",asyncHandler(async (req,res) => {
-
-   await Product.remove()
-
-    res.json({
-        msg : "deleted all product"
-    })
-}))
+router.delete( "/", deleteProduct)
 
 //product 특정 delete
-router.delete("/:productId", asyncHandler(async (req,res) => {
-
-    const {productId} = req.params
-    // const id = req.params.productId
-    await Product.findByIdAndRemove(productId)
-
-    res.json({
-        msg : "deleted at "+ productId
-    })
-
-}))
+router.delete("/:productId", deleteProductDetail )
 
 export default router
