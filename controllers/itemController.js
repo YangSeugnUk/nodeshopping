ce7205829a598c8a00c166e82f70e3ecc3aa87a3
Yang <9488372@hanmail.net>
2022-04-17 10:25:41 +0900

    add itemController

---
+++ controllers/itemController.js
@@ -0,0 +1,95 @@
+import asyncHandler from "express-async-handler";
+import Item from "../models/itemModel.js";
+
+const itemGet = asyncHandler(async (req, res) => {
+    const items = await Item.find()
+    res.json({
+        count: items.length,
+        items
+    })
+
+})
+
+const itemGetDetail = asyncHandler( async (req,res) => {
+
+    const {itemId} = req.params
+
+    const item = await Item.findById(itemId)
+
+    res.json(item)
+
+
+})
+
+const itemPost = asyncHandler(async (req,res) => {
+    const {name, price, category, product, brand, paymentMethod} = req.body
+
+    const item = new Item({
+        name,
+        price,
+        category,
+        product,
+        brand,
+        paymentMethod
+    })
+
+    const newItem = await item.save();
+
+    res.json({
+        message: "create a item",
+        item : newItem
+    })
+
+})
+
+const itemPut = asyncHandler(async (req,res) => {
+
+    const {itemId} = req.params
+    const {name, price, category, product, brand, paymentMethod} = req.body
+
+    const item = await Item.findById(itemId)
+
+    if(item){
+        item.name = name || item.name
+        item.price = price || item.price
+        item.category = category || item.category
+        item.product = product || item.product
+        item.brand = brand || item.brand
+        item.paymentMethod = paymentMethod || item.paymentMethod
+
+        const updateItem = await item.save()
+
+        res.json({
+            msg : "update item",
+            item : updateItem
+        })
+    }
+})
+
+const itemDelete = asyncHandler( async (req,res) => {
+    await Item.remove()
+
+    res.json({
+        msg : "deleted all item"
+    })
+
+})
+
+const itemDeleteDetail = asyncHandler(async (req,res) => {
+    const {itemId} = req.params
+    await Item.findByIdAndRemove(itemId)
+
+    res.json({
+        msg : "delete at " + itemId
+    })
+
+})
+
+
+export {itemGet,
+    itemGetDetail,
+    itemPost,
+    itemPut,
+    itemDelete,
+    itemDeleteDetail
+}
--- routes/item.js
+++ routes/item.js
@@ -1,106 +1,36 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
 import Item from "../models/itemModel.js";
+import {itemGet,
+    itemGetDetail,
+    itemPost,
+    itemDelete,
+    itemDeleteDetail
+
+} from "../controllers/itemController.js";
 
 const router = express.Router()
 
 // crud
 
 // item 전체불러오는 api
-router.get("/", asyncHandler(async (req, res) => {
-    const items = await Item.find()
-    res.json({
-        count: items.length,
-        items
-    })
-
-}))
+router.get("/", itemGet)
 
 
 // item 상세불러오기 api
-router.get("/:itemId", asyncHandler( async (req,res) => {
-
-    const {itemId} = req.params
-
-    const item = await Item.findById(itemId)
-
-    res.json(item)
-
-
-}))
+router.get("/:itemId", itemGetDetail)
 
 // item 등록해주는 api
-router.post("/", asyncHandler(async (req,res) => {
-    const {name, price, category, product, brand, paymentMethod} = req.body
-
-    const item = new Item({
-        name,
-        price,
-        category,
-        product,
-        brand,
-        paymentMethod
-    })
-
-    const newItem = await item.save();
-
-    res.json({
-        message: "create a item",
-        item : newItem
-    })
-
-}))
+router.post("/", itemPost)
 
 // item 업데이트해주는 api
-router.put("/:itemId", asyncHandler(async (req,res) => {
-
-    const {itemId} = req.params
-    const {name, price, category, product, brand, paymentMethod} = req.body
-
-    const item = await Item.findById(itemId)
-
-    if(item){
-        item.name = name || item.name
-        item.price = price || item.price
-        item.category = category || item.category
-        item.product = product || item.product
-        item.brand = brand || item.brand
-        item.paymentMethod = paymentMethod || item.paymentMethod
-
-        const updateItem = await item.save()
-
-        res.json({
-            msg : "update item",
-            item : updateItem
-        })
-    }
-
-
-
-
-}))
+router.put("/:itemId", itemPut )
 
 // item 전체삭제해주는 api
-router.delete("/", asyncHandler( async (req,res) => {
-    await Item.remove()
-
-    res.json({
-        msg : "deleted all item"
-    })
-
-}))
+router.delete("/", itemDelete)
 
 // item 특정삭제해주는 api
-router.delete("/:itemId", asyncHandler(async (req,res) => {
-    const {itemId} = req.params
-    await Item.findByIdAndRemove(itemId)
-
-    res.json({
-        msg : "delete at " + itemId
-    })
-
-}))
-
+router.delete("/:itemId", itemDeleteDetail)
 
 export default router
 
