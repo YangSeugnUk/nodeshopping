2f024c51bb0f75a66f4e09d409669f6ccfbe7c89
Yang <9488372@hanmail.net>
2022-04-17 10:49:12 +0900

    add product pagenation and keyword

--- controllers/productController.js
+++ controllers/productController.js
@@ -3,17 +3,28 @@ import Product from "../models/productModel.js";
 
 
 const getProducts = asyncHandler (async (req, res) => {
-    const products = await Product.find()
+    const pageSize = 5
+    const page = Number(req.query.pageNumber) || 1
+    const keyword = req.query.keyword
+        ? {
+            name: {
+                $regex: req.query.keyword,
+                $options: 'i'
+            }
+        }
+        : {}
+
+    const count = await Product.countDocuments({...keyword})
+
+    const products = await Product
+        .find()
+        .limit(pageSize)
+        .skip(pageSize * (page - 1))
+
     res.json({
-        count: products.length,
-        // products: products.map(product => {
-        //     return {
-        //         id: product._id,
-        //         name: product.name,
-        //         price: product.price,
-        //     }
-        // })
-        products
+        products,
+        page,
+        pages : Math.ceil(count/pageSize)
     })
 })
 
